9315e015535f17200e2d3e28c3714c5eac46248d
Fikri Yusrihan <47127883+fikriyusrihan@users.noreply.github.com>
2023-08-09 23:16:35 +0700

    fix: websocket connection on comments

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 /* eslint-disable no-console */
 import mongoose from 'mongoose';
 import { Server } from 'socket.io';
+import * as http from 'http';
 import app from './infrastructures/express/index.js';
 import config from './utils/config.js';
 
@@ -12,12 +13,31 @@ mongoose.connect(config.MONGO_URI).then(() => {
   console.log('Connected to MongoDB');
   mongoose.set('debug', true);
 
-  server = app.listen(config.PORT, () => {
-    console.log(`Listening to port ${config.PORT}`);
+  server = http.createServer(app);
+
+  const io = new Server(server, {
+    cors: {
+      origin: '*',
+    },
+    path: '/api/v1/comments',
   });
 
-  const io = new Server(server);
+  io.on('connection', (socket) => {
+    console.log('A user connected');
+
+    const { videoId } = socket.handshake.query;
+    socket.join(videoId);
+
+    socket.on('disconnect', () => {
+      console.log('A user disconnected');
+    });
+  });
+
   app.set('io', io);
+
+  server.listen(config.PORT, () => {
+    console.log(`Listening to port ${config.PORT}`);
+  });
 });
 
 const exitHandler = () => {
