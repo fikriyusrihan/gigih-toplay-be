87065c1bb368b4fe61edfff100cb10f4ff8ee15b
Fikri Yusrihan <47127883+fikriyusrihan@users.noreply.github.com>
2023-08-12 10:01:01 +0700

    add rate limiter

--- src/infrastructures/express/index.js
+++ src/infrastructures/express/index.js
@@ -1,13 +1,25 @@
 import express from 'express';
 import swaggerUi from 'swagger-ui-express';
 import cors from 'cors';
+import rateLimit from 'express-rate-limit';
 import swaggerDocument from '../../docs/index.js';
 import routes from './routes/index.js';
 import { errorConverter, errorHandler } from './middlewares/error.js';
 
 const app = express();
+
+const limiter = rateLimit({
+  windowMs: 2 * 60 * 1000, // 2 minutes
+  max: 200, // 100 requests per 2 minutes
+  standardHeaders: true,
+  legacyHeaders: false,
+});
+
 app.use(express.json());
 app.use(cors());
+
+// Use rate limiter to limit requests
+app.use(limiter);
 
 app.use(
   '/api/v1/docs',
